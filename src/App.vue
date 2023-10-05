a54feec08399c72d3731d7aa63762b08bf93b003
vermataruna <tarunaverma792@gmail.com>
2023-10-06 00:16:04 +0530

    add: card component

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,7 @@
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div class="card" v-for="quiz in quizes" :key="quiz.id">
-        <img :src="quiz.img">
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }}</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
     </div>
   </div>
 </template>
@@ -19,6 +13,7 @@
 <script setup>
 import quizData from '../src/data/quizes.json';
 import { ref, watch } from 'vue';
+import Card from '../src/components/Card.vue';
 
 const quizes = ref(quizData);
 const search = ref("");
@@ -61,30 +56,6 @@ header input {
   flex-wrap: wrap;
   margin-top: 40px;
 }
-
-.card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
 
   .options-container {
     display: flex;
